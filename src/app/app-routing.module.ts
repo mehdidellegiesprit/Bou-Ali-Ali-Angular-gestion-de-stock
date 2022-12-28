43dd21441ac5bd3e2a57c579b63ccda7dd64fb88
mehdi <mehdi.dellegi@esprit.tn>
2022-12-28 10:26:15 -0800

    First commit angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,150 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
+import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageStatistiquesComponent} from "./pages/page-statistiques/page-statistiques.component";
+import {PageArticleComponent} from "./pages/articles/page-article/page-article.component";
+import {NouvelArticleComponent} from "./pages/articles/nouvel-article/nouvel-article.component";
+import {PageMvtstkComponent} from "./pages/mvtstk/page-mvtstk/page-mvtstk.component";
+import {PageClientComponent} from "./pages/client/page-client/page-client.component";
+import {PageFournisseurComponent} from "./pages/fournisseur/page-fournisseur/page-fournisseur.component";
+import {NouveauCltFrsComponent} from "./composants/nouveau-clt-frs/nouveau-clt-frs.component";
+import {PageCmdCltFrsComponent} from "./pages/page-cmd-clt-frs/page-cmd-clt-frs.component";
+import {NouvelleCmdCltFrsComponent} from "./composants/nouvelle-cmd-clt-frs/nouvelle-cmd-clt-frs.component";
+import {PageCategoriesComponent} from "./pages/categories/page-categories/page-categories.component";
+import {NouvelleCategoryComponent} from "./pages/categories/nouvelle-category/nouvelle-category.component";
+import {PageUtilisateurComponent} from "./pages/utilisateur/page-utilisateur/page-utilisateur.component";
+import {NouvelUtilisateurComponent} from "./pages/utilisateur/nouvel-utilisateur/nouvel-utilisateur.component";
+import {PageProfilComponent} from "./pages/profil/page-profil/page-profil.component";
+import {ChangerMotDePasseComponent} from "./pages/profil/changer-mot-de-passe/changer-mot-de-passe.component";
+import {ApplicationGuardService} from "./services/guard/application-guard.service";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:PageLoginComponent
+  },
+  {
+    path:'inscrire',
+    component:PageInscriptionComponent
+  },
+  {
+    path:'',
+    component:PageDashboardComponent,
+    children:[
+      {
+        path:'statistiques',
+        component:PageStatistiquesComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'articles',
+        component:PageArticleComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'nouvelarticle',
+        component:NouvelArticleComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'mvtstk',
+        component:PageMvtstkComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'clients',
+        component:PageClientComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'nouveauclient',
+        component:NouveauCltFrsComponent,
+        canActivate:[ApplicationGuardService],
+        data:{
+          origin:'client'
+        }
+      },
+      {
+        path:'commandesclient',
+        component:PageCmdCltFrsComponent,
+        canActivate:[ApplicationGuardService],
+        data:{
+          origin:'clients'
+        }
+      },
+      {
+        path:'nouvellecommandeclt',
+        component:NouvelleCmdCltFrsComponent,
+        canActivate:[ApplicationGuardService],
+        data:{
+          origin:'client'
+        }
+      },
+      {
+        path:'fournisseurs',
+        component:PageFournisseurComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'nouveaufournisseur',
+        component:NouveauCltFrsComponent,
+        canActivate:[ApplicationGuardService],
+        data:{
+          origin:'fournisseur'
+        }
+      },
+      {
+        path:'commandesfournisseur',
+        component:PageCmdCltFrsComponent,
+        canActivate:[ApplicationGuardService],
+        data:{
+          origin:'fournisseurs'
+        }
+      },
+      {
+        path:'nouvellecommandefrs',
+        component:NouvelleCmdCltFrsComponent,
+        canActivate:[ApplicationGuardService],
+        data:{
+          origin:'fournisseur'
+        }
+      },
+      {
+        path:'categories',
+        component:PageCategoriesComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'nouvellecategorie',
+        component:NouvelleCategoryComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'utilisateurs',
+        component:PageUtilisateurComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'nouvelutilisateur',
+        component:NouvelUtilisateurComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'profil',
+        component:PageProfilComponent,
+        canActivate:[ApplicationGuardService]
+      },
+      {
+        path:'changermotdepasse',
+        component:ChangerMotDePasseComponent,
+        canActivate:[ApplicationGuardService]
+      }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
